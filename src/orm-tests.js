f059f471b2a38eb8303f509d6bd6beb8f336de03
David Luecke <daff@neyeon.com>
2016-09-05 14:15:54 -0700

    Upgrade to Mocha 3.x

--- src/orm-tests.js
+++ src/orm-tests.js
@@ -4,11 +4,10 @@ import { expect } from 'chai';
 
 export default function orm(people, _ids, errors) {
   describe('Feathers ORM Specific Tests', () => {
-    it('wraps an ORM error in a feathers error', done => {
-      people.create({}).catch(error => {
-        expect(error instanceof errors.FeathersError).to.be.ok;
-        done();
-      });
+    it('wraps an ORM error in a feathers error', () => {
+      return people.create({}).catch(error =>
+        expect(error instanceof errors.FeathersError).to.be.ok
+      );
     });
   });
 }
--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,10 @@ import assert from 'assert';
 import * as plugin from '../src';
 
 describe('feathers-service-tests', () => {
-  it('basic functionality', done => {
+  it('basic functionality', () => {
     assert.equal(typeof plugin, 'object', 'plugin is not an object');
     assert.equal(typeof plugin.base, 'function', 'plugin.base is not a function');
     assert.equal(typeof plugin.example, 'function', 'plugin.example is not a function');
     assert.equal(typeof plugin.orm, 'function', 'plugin.orm is not a function');
-    done();
   });
 });
